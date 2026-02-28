9f5b325c0c0fbd77d2b53a079f36942052e42f3a
unknown <TT616@ccs.local>
2026-02-28 18:18:36 +0800

    Improving UI design and adding landing page

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-const toggle = document.getElementById('dropdownToggle');
-const loginBtn = document.getElementById('loginItem');
-const registerBtn = document.getElementById('registerItem');
+// const toggle = document.getElementById('dropdownToggle');
+// const loginBtn = document.getElementById('loginItem');
+// const registerBtn = document.getElementById('registerItem');
 
-// Function to update the text and then navigate
-loginBtn.addEventListener('click', function(e) {
-    e.preventDefault(); 
-    toggle.textContent = 'Login';
+// // Function to update the text and then navigate
+// loginBtn.addEventListener('click', function(e) {
+//     e.preventDefault(); 
+//     toggle.textContent = 'Login';
     
-    // Manually tell the browser to change the page
-    window.location.href = 'login.html'; 
-});
+//     // Manually tell the browser to change the page
+//     window.location.href = 'login.html'; 
+// });
 
-registerBtn.addEventListener('click', function(e) {
-    e.preventDefault(); 
-    toggle.textContent = 'Register';
+// registerBtn.addEventListener('click', function(e) {
+//     e.preventDefault(); 
+//     toggle.textContent = 'Register';
     
-    // Manually tell the browser to change the page
-    window.location.href = 'register.html';
-});
+//     // Manually tell the browser to change the page
+//     window.location.href = 'register.html';
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const loginForm = document.getElementById('loginForm');
+// document.addEventListener("DOMContentLoaded", function () {
+//     const loginForm = document.getElementById('loginForm');
 
-    // Handle the form submission
-    loginForm.addEventListener('submit', function (e) {
-        e.preventDefault(); // Prevent the default form submission behavior
+//     // Handle the form submission
+//     loginForm.addEventListener('submit', function (e) {
+//         e.preventDefault(); // Prevent the default form submission behavior
 
-        // Simulate successful login by setting a flag in localStorage
-        localStorage.setItem('isLoggedIn', 'true'); // Save the login state
+//         // Simulate successful login by setting a flag in localStorage
+//         localStorage.setItem('isLoggedIn', 'true'); // Save the login state
 
-        // Redirect to home page (temporary simulation of a successful login)
-        window.location.href = 'home.html'; // Redirects the user to home.html
-    });
-});
+//         // Redirect to home page (temporary simulation of a successful login)
+//         window.location.href = 'home.html'; // Redirects the user to home.html
+//     });
+// });
