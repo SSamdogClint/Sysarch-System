4abf751a6679b29a58289d29d002aa0cefedc117
SSamdogClint <capondag.clint117@gmail.com>
2026-03-20 23:09:09 +0800

    adding reset session function for students credit session

---
+++ reset_sessions.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+require_once 'config/db_config.php';
+header('Content-Type: application/json');
+
+if (empty($_SESSION['admin_logged_in'])) {
+    echo json_encode(['success' => false, 'message' => 'Unauthorized.']);
+    exit;
+}
+
+$conn->query('UPDATE students SET session_credits = 30');
+
+echo json_encode(['success' => true]);
